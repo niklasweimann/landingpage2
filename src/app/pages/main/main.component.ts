e6cc7a3a78d52e3a8a376e88c3f51f1e0d359dea
Niklas Weimann <nikweimann@gmail.com>
2022-10-22 11:38:27 +0200

    Improve for mobile; Fix Font; Fix service links

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -10,32 +10,72 @@ import {Title} from "@angular/platform-browser";
 })
 export class MainComponent implements OnInit {
   @ViewChildren('section') private sections: any;
-  private subscription = new Subscription();
+  private subscriptions: Subscription[] = [];
   private isFirefox = false;
   private currentSlideIndex = 0;
   private slidesCount: number = 0;
   private ticking = false;
 
-  constructor(private titleService:Title) {
+  constructor(private titleService: Title) {
     this.titleService.setTitle("Niklas Weimann")
   }
 
   public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach(x => x.unsubscribe());
   }
 
   public ngOnInit(): void {
-    this.isFirefox = (/Firefox/i.test(navigator.userAgent));
-    const eventName = this.isFirefox ? 'DOMMouseScroll' : 'wheel';
-    this.subscription = fromEvent<any>(window, eventName).pipe(debounceTime(100)).subscribe((event) => {
-      this.handleScrollEvent(event);
+    const userAgent = navigator.userAgent;
+    this.isFirefox = /Firefox/i.test(userAgent);
+    this.subscriptions.push(fromEvent<any>(window, 'touchstart').pipe(debounceTime(100)).subscribe((event) => {
+      const firstTouch = event.touches[0];
+      this.xDown = firstTouch.clientX;
+      this.yDown = firstTouch.clientY;
+    }));
+    this.subscriptions.push(fromEvent<any>(window, 'touchmove').pipe(debounceTime(100)).subscribe((event) => {
+      this.handleTouchMove(event);
       event.preventDefault();
-    });
+    }));
+    this.subscriptions.push(
+      fromEvent<any>(window, this.isFirefox ? 'DOMMouseScroll' : 'wheel')
+        .pipe(debounceTime(100)).subscribe((event) => {
+        this.handleScrollEvent(event);
+        event.preventDefault();
+      })
+    );
   }
+
+  private xDown: number | null = null;
+  private yDown: number | null = null;
+
+  private handleTouchMove(evt: TouchEvent) {
+    this.slidesCount = this.sections.toArray().length;
+    if (!this.xDown || !this.yDown) {
+      return;
+    }
+
+    const xUp = evt.touches[0].clientX;
+    const yUp = evt.touches[0].clientY;
+
+    const xDiff = this.xDown - xUp;
+    const yDiff = this.yDown - yUp;
+
+    if (Math.abs(xDiff) <= Math.abs(yDiff)) {
+      if (yDiff > 0) {
+        this.scrollDown();
+      } else {
+        this.scrollUp();
+      }
+    }
+
+    this.xDown = null;
+    this.yDown = null;
+  };
 
   public handleScrollEvent(event: WheelEvent): void {
     this.slidesCount = this.sections.toArray().length;
     let delta: number;
+    const deltaLimit = 30;
     if (this.isFirefox) {
       delta = event.detail * (-120);
     } else {
@@ -43,25 +83,31 @@ export class MainComponent implements OnInit {
     }
 
     if (!this.ticking) {
-      if (delta <= -30) {
-        // Down scroll
-        this.ticking = true;
-        if (this.currentSlideIndex !== this.slidesCount - 1) {
-          this.currentSlideIndex++;
-          this.nextItem();
-        }
-        this.slideDurationTimeout();
+      if (delta <= -deltaLimit) {
+        this.scrollDown();
       }
-      if (delta >= 30) {
-        // Up scroll
-        this.ticking = true;
-        if (this.currentSlideIndex !== 0) {
-          this.currentSlideIndex--;
-        }
-        this.previousItem();
-        this.slideDurationTimeout();
+      if (delta >= deltaLimit) {
+        this.scrollUp();
       }
     }
+  }
+
+  private scrollDown(): void {
+    this.ticking = true;
+    if (this.currentSlideIndex !== this.slidesCount - 1) {
+      this.currentSlideIndex++;
+      this.nextItem();
+    }
+    this.slideDurationTimeout();
+  }
+
+  private scrollUp(): void {
+    this.ticking = true;
+    if (this.currentSlideIndex !== 0) {
+      this.currentSlideIndex--;
+    }
+    this.previousItem();
+    this.slideDurationTimeout();
   }
 
   private nextItem(): void {
