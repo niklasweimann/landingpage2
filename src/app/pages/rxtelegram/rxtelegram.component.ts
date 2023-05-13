4a95197af8377db5682c211e6bf023c22591293f
Niklas Weimann <nikweimann@gmail.com>
2023-05-13 22:33:50 +0200

    Add rxtelegram page

---
+++ src/app/pages/rxtelegram/rxtelegram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rxtelegram',
+  templateUrl: './rxtelegram.component.html',
+  styleUrls: ['./rxtelegram.component.scss']
+})
+export class RxtelegramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
